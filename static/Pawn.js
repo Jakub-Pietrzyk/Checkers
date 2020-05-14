d448088beff06636731d5cfde65f03fc6ade0e34
Jakub Pietrzyk <jakub.pietrzyk01@gmail.com>
2020-05-14 14:30:08 +0200

    [NEW] Highlight possible fields

--- static/Pawn.js
+++ static/Pawn.js
@@ -18,13 +18,36 @@ class Pawn extends THREE.Mesh {
     }
 
     setActivePawn(){
+      Object.keys(game.moveable_fields).forEach(function(e){
+        game.scene.getObjectByName(e).material.color.set("#000000");
+      })
+
       if(this != ui.active_pawn){
         for(var i=0;i< game.pawns[net.player_color].length;i++){
           game.pawns[net.player_color][i].material.color.set(game.COLORS[net.player_color].color)
         }
         this._color = "#FFA500"
         this.material.color.set("#FFA500")
-        ui.active_pawn = this
+        ui.active_pawn = this;
+
+        var correctRow
+        var correctColumns = [];
+        if(net.player_color == "black") correctRow = this.row + 1
+        if(net.player_color == "white") correctRow = this.row - 1
+        var is_friendly_right = false;
+        var is_friendly_left = false;
+        for(var i=0;i<game.pawns[net.player_color].length;i++){
+          if (game.pawns[net.player_color][i].row == correctRow && game.pawns[net.player_color][i].column == (this.column + 1)) is_friendly_right = true;
+        }
+        for(var i=0;i<game.pawns[net.player_color].length;i++){
+          if (game.pawns[net.player_color][i].row == correctRow && game.pawns[net.player_color][i].column == (this.column - 1)) is_friendly_left = true;
+        }
+        if(!is_friendly_right) correctColumns.push(this.column +1);
+        if(!is_friendly_left) correctColumns.push(this.column -1);
+        for(var i=0;i<correctColumns.length;i++){
+          if(correctColumns[i] >= 0 && correctColumns[i] <= 7)game.scene.getObjectByName(correctRow + "-" + correctColumns[i]).material.color.set("#A5FF00");
+        }
+
       } else {
         ui.active_pawn = null;
         this.material.color.set(game.COLORS[net.player_color].color)
@@ -32,7 +55,9 @@ class Pawn extends THREE.Mesh {
     }
 
     movePawn(field){
-      var correctRows = [this.row + 1, this.row - 1]
+      var correctRows = [];
+      if(net.player_color == "black") correctRows.push(this.row + 1);
+      if(net.player_color == "white") correctRows.push(this.row - 1);
       var correctColumns = [this.column + 1, this.column - 1]
       var field_data = game.moveable_fields[field.name]
       var old_row = this.row;
@@ -56,6 +81,9 @@ class Pawn extends THREE.Mesh {
           }
           ui.active_pawn = null;
           this.material.color.set(game.COLORS[net.player_color].color);
+          Object.keys(game.moveable_fields).forEach(function(e){
+            game.scene.getObjectByName(e).material.color.set("#000000");
+          })
           net.updateGameArray();
         }
       }
